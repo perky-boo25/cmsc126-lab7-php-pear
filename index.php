ad47e9885f3a4c6deab289b83fb734be09de57ac
perky-boo25 <samaniegopercie055@gmail.com>
2026-04-24 17:09:44 +0800

    Adding record manangement in index.php

--- index.php
+++ index.php
@@ -73,8 +73,28 @@
         <td class="tlabel"></td>
         <td><br><input type="submit" value="Register Student"></td>
       </tr>
-
     </table>
+  </form>
+  <h2>Record Management</h2>
+  <p><b>Look Up by Student ID</b></p>
+
+  <input type="text" id="student_id_input" placeholder="Enter Student ID">
+
+  <!--TODO: Search function, add filename inside quotations in form action -->
+  <form action="search.php" method="get" style="display:inline;">
+    <input type="hidden" name="student_id" id="search_id">
+    <input type="submit" value="Search" onclick="document.getElementById('search_id').value = document.getElementById('student_id_input').value">
+  </form>
+
+<!--TODO: Update the info, add filename inside quotations in form action -->
+  <form action="" method="get" style="display:inline;">
+    <input type="hidden" name="student_id" id="update_id">
+    <input type="submit" value="Update" onclick="document.getElementById('update_id').value = document.getElementById('student_id_input').value">
+  </form>
+
+  <form action="delete.php" method="post" style="display:inline;">
+    <input type="hidden" name="student_id" id="delete_id">
+    <input type="submit" value="Delete" onclick="document.getElementById('delete_id').value = document.getElementById('student_id_input').value">
   </form>
 </body>
 </html>
